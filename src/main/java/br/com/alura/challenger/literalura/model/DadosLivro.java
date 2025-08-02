068b9b5e871ca2a81416de2e0467cd9ab6b14a2c
RicardoLMessias <ricardo.lima.messias@gmail.com>
2025-08-02 09:19:58 -0300

    finalizado

--- src/main/java/br/com/alura/challenger/literalura/model/DadosLivro.java
+++ src/main/java/br/com/alura/challenger/literalura/model/DadosLivro.java
@@ -8,12 +8,7 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public record DadosLivro(
-//        @Id
-//        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        Long id,
-//        @Column(unique = true)
         @JsonAlias("title") String titulo,
-//        @Column(name = "Autor")
         List<DadosAutor> authors,
         @JsonAlias("languages")List<String> idioma,
         @JsonAlias("download_count") Integer downloads
